a1df7a0dfba876502f5a191430fe4083cb15acdb
Gustavodeoliveiraa <ggu3131@gmail.com>
2024-10-20 21:24:30 -0300

    feat: added settings of .env

--- project/settings/database.py
+++ project/settings/database.py
@@ -1,8 +1,24 @@
+import os
 from .environment import BASE_DIR
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+debug_mode = os.environ.get('DEBUG')
+
+if debug_mode == '1':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+
+if debug_mode == '0':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('DATABASE_NAME'),
+            'USER': os.environ.get('USER'),
+            'PASSWORD': os.environ.get('PASSWORD'),
+            'HOST': os.environ.get('HOST'),
+            'PORT': os.environ.get('PORT')
+        }
+    }
--- project/settings/environment.py
+++ project/settings/environment.py
@@ -1,10 +1,14 @@
+import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 SECRET_KEY = 'django-insecure-(0+0tm+s^7x#y+0)eu5(+)foo$vl0p)tv$!jojd3g5&l%_9ir*'
 
-DEBUG = True
+DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
 ALLOWED_HOSTS = ['*']
 

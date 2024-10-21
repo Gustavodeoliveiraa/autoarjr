8b2dfd572a491461b92d69357033ef5168c1c41d
Gustavodeoliveiraa <ggu3131@gmail.com>
2024-10-21 02:00:10 -0300

    defult url changed

--- project/settings/assets.py
+++ project/settings/assets.py
@@ -2,9 +2,5 @@ import os
 from project.settings.environment import BASE_DIR
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'client/static'),
-]
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

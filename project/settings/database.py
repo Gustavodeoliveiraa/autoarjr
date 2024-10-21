5aa02cd626cd0ea8e5b9477be627f8de6d1abc73
Gustavodeoliveiraa <ggu3131@gmail.com>
2024-10-20 22:46:09 -0300

    added print()

--- project/settings/database.py
+++ project/settings/database.py
@@ -20,8 +20,8 @@ if debug_mode == '0':
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': os.environ.get('DATABASE_NAME'),
             'USER': os.environ.get('DATABASE_USER'),
-            'PASSWORD': os.environ.get('PASSWORD'),
-            'HOST': os.environ.get('HOST'),
-            'PORT': os.environ.get('PORT')
+            'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+            'HOST': os.environ.get('DATABASE_HOST'),
+            'PORT': os.environ.get('DATABASE_PORT'),
         }
     }

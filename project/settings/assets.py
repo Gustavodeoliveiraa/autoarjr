189a8f99373afe418a583625c50ce4d50ef07bc5
Gustavodeoliveiraa <ggu3131@gmail.com>
2024-10-20 21:45:35 -0300

    refactor: added STATIC_ROOT

--- project/settings/assets.py
+++ project/settings/assets.py
@@ -1,3 +1,7 @@
+import os
+from project.settings.environment import BASE_DIR
+
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

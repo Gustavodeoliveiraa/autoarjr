a6bc82448cc05f288ce600b630a8fd45c2908108
Gustavodeoliveiraa <ggu3131@gmail.com>
2024-10-26 18:44:09 -0300

    feature: added metrics for sales

--- project/urls.py
+++ project/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from .views import DashBoardView
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', DashBoardView.as_view(), name='dashboard'),
     path('', include('storage.urls')),
     path('', include('client.urls')),
     path('', include('service_order.urls')),
---
+++ project/views.py
@@ -0,0 +1,20 @@
+from django.views import View
+from django.shortcuts import render
+from project import metrics
+
+
+class DashBoardView(View):
+    template_name = '../templates/metrics/dashboard.html'
+
+    def get(self, request):
+        data = {
+            'total_of_cars_made': metrics.get_total_amount_of_cars_made(),
+            'total_selling': metrics.get_total_amount_selling(),
+            'total_cpf_registered': metrics.get_total_amount_of_cps_registered(),
+            'total_clients_registered': metrics.get_total_amount_of_clients_registered(),
+
+            'total_value_of_sales_for_day_of_week': metrics.get_total_amount_of_made_on_week(),
+            'sales_by_day_of_the_week': metrics.get_total_amount_of_cars_made_per_day_on_the_last_7_days(),
+        }
+
+        return render(request, self.template_name, data)

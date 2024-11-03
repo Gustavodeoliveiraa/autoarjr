3b50b6332de013d4a7b3774e407ad4a3a2e6b974
Gustavodeoliveiraa <ggu3131@gmail.com>
2024-11-03 04:34:28 -0300

    style: added new style for the side menu

--- project/settings/assets.py
+++ project/settings/assets.py
@@ -4,4 +4,8 @@ from project.settings.environment import BASE_DIR
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'project/static'),
+]
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
